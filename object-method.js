ae1924661ac6e7b68f8ad88087d41ebc24a97e25
shahinexy <shahinexy@gmail.com>
2023-12-05 11:47:41 +0600

    added object method file

---
+++ object-method.js
@@ -0,0 +1,33 @@
+// ===================
+//    Object Methods
+// ===================
+
+// ---object key()---
+
+// const myObject = {
+//     userName: 'Shahin Alam',
+//     id: 1231,
+//     isStudent: true,
+// }
+// const resultKey = Object.keys(myObject);
+// console.log(resultKey);
+
+// ---object vlaue---
+
+// const myObject = {
+//     userName: 'Shahin Alam',
+//     id: 1231,
+//     isStudent: true,
+// }
+// const resulValue = Object.values(myObject);
+// console.log(resulValue);
+
+// ---Intries---
+
+const myObject = {
+    userName: 'Shahin Alam',
+    id: 1231,
+    isStudent: true,
+}
+const resulEntries = Object.entries(myObject);
+console.log(resulEntries);
