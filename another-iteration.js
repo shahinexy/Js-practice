9e6a023fd60a6d01d702124a678fb931b65be0b1
shahinexy <shahinexy@gmail.com>
2023-12-17 11:44:07 +0600

    another iteration

---
+++ another-iteration.js
@@ -0,0 +1,20 @@
+// const myObj = {
+//     Name: 'Shahin',
+//     fFruit: ['Mango', 'Apple', 'Banana'],
+//     printfFruit: function(){
+//         this.fFruit.forEach(function(e){
+//             console.log(`${this.Name} like ${e}`);
+//         })
+//     }
+// }
+// myObj.printfFruit();
+
+
+const myObj = {
+    Name: 'Shahin',
+    fFruit: ['Mango', 'Apple', 'Banana'],
+    printfFruit: function(){
+        this.fFruit.forEach((e) => console.log(`${this.Name} like ${e}`))
+    }
+}
+myObj.printfFruit();
