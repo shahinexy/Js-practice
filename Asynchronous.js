83d20a780bbc4a1cf91649788b43f4c7847c3d89
shahinexy <shahinexy@gmail.com>
2023-12-17 18:50:25 +0600

    callback

--- Asynchronous.js
+++ Asynchronous.js
@@ -7,8 +7,31 @@
 // console.log('Line 3 code');
 
 // ================== JS Asynchronous behavior ==============
-console.log('Line 1 code');
-setTimeout(function(){
-    console.log('Line 2 code');
-}, 2000)
-console.log('Line 3 code');
+// console.log('Line 1 code');
+// setTimeout(function(){
+//     console.log('Line 2 code');
+// }, 2000)
+// console.log('Line 3 code');
+
+
+// =========== Callback Function =============
+function display(some){
+    console.log(some);
+}
+
+function myFunction(x, y,){
+    const sum = x+y;
+    return sum
+}
+const result = myFunction(5,5);
+display(result);
+
+
+// function display(){
+//     console.log(sum);
+// }
+
+// function myFunction(x, y, callback){
+//     const sum = x+y;
+//     return sum
+// }
