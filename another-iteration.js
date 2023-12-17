57ed874e7085419cdcae1c8bc6df91728fa827be
shahinexy <shahinexy@gmail.com>
2023-12-17 13:24:03 +0600

    added this

--- another-iteration.js
+++ another-iteration.js
@@ -9,6 +9,18 @@
 // }
 // myObj.printfFruit();
 
+// ========= if we use this as a another peramiter it will work again =============
+// const myObj = {
+//     Name: 'Shahin',
+//     fFruit: ['Mango', 'Apple', 'Banana'],
+//     printfFruit: function(){
+//         this.fFruit.forEach(function(e){
+//             console.log(`${this.Name} like ${e}`);
+//         }, this)
+//     }
+// }
+// myObj.printfFruit();
+
 // ============================================================================
 
 // const myObj = {
